3426902c4619d97349c33f5aae3e1be6b31f0e54
Rizal Widyarta Gowandy <rizal.gowandy@tokopedia.com>
2020-10-25 13:58:36 +0700

    Add Loader

---
+++ loader.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	_ "github.com/peractio/gdk/pkg/converter"
+	_ "github.com/peractio/gdk/pkg/regex"
+	_ "github.com/peractio/gdk/pkg/validator"
+)
+
+// Ensure all package can be built correctly.
+func main() {}
