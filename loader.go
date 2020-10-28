30747c2a0f4f94715fb951edfe871d83395f65bd
Rizal Widyarta Gowandy <rizal.gowandy@tokopedia.com>
2020-10-28 10:41:19 +0700

    Add More Package to loader.go

--- loader.go
+++ loader.go
@@ -3,6 +3,10 @@ package main
 import (
 	_ "github.com/peractio/gdk/examples/myapp"
 	_ "github.com/peractio/gdk/pkg/converter"
+	_ "github.com/peractio/gdk/pkg/env"
+	_ "github.com/peractio/gdk/pkg/errorx"
+	_ "github.com/peractio/gdk/pkg/ternary"
+	_ "github.com/peractio/gdk/pkg/try"
 	_ "github.com/peractio/gdk/pkg/validator"
 )
 
