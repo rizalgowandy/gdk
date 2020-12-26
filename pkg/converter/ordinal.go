1cd653d7ffdeab23a4b03f63e15dfdf0b4266129
Rizal Widyarta Gowandy <rizal.gowandy@tokopedia.com>
2020-12-26 12:49:59 +0700

    Add Converter to Ordinal

---
+++ pkg/converter/ordinal.go
@@ -0,0 +1,35 @@
+package converter
+
+import (
+	"strconv"
+)
+
+const (
+	firstOrdinal  = 1
+	secondOrdinal = 2
+	thirdOrdinal  = 3
+
+	firstOrdinalDivider  = 11
+	secondOrdinalDivider = 12
+	thirdOrdinalDivider  = 13
+)
+
+// ToOrdinal get ordinal 1st, 2nd, 3rd, etc.
+func ToOrdinal(x int) string {
+	suffix := "th"
+	switch x % 10 {
+	case firstOrdinal:
+		if x%100 != firstOrdinalDivider {
+			suffix = "st"
+		}
+	case secondOrdinal:
+		if x%100 != secondOrdinalDivider {
+			suffix = "nd"
+		}
+	case thirdOrdinal:
+		if x%100 != thirdOrdinalDivider {
+			suffix = "rd"
+		}
+	}
+	return strconv.Itoa(x) + suffix
+}
---
+++ pkg/converter/ordinal_test.go
@@ -0,0 +1,94 @@
+package converter
+
+import (
+	"testing"
+)
+
+func TestToArrInt2(t *testing.T) {
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "1st",
+			args: args{
+				x: 1,
+			},
+			want: "1st",
+		},
+		{
+			name: "2nd",
+			args: args{
+				x: 2,
+			},
+			want: "2nd",
+		},
+		{
+			name: "3rd",
+			args: args{
+				x: 3,
+			},
+			want: "3rd",
+		},
+		{
+			name: "11th",
+			args: args{
+				x: 11,
+			},
+			want: "11th",
+		},
+		{
+			name: "22nd",
+			args: args{
+				x: 22,
+			},
+			want: "22nd",
+		},
+		{
+			name: "33rd",
+			args: args{
+				x: 33,
+			},
+			want: "33rd",
+		},
+		{
+			name: "21st",
+			args: args{
+				x: 21,
+			},
+			want: "21st",
+		},
+		{
+			name: "101st",
+			args: args{
+				x: 101,
+			},
+			want: "101st",
+		},
+		{
+			name: "202nd",
+			args: args{
+				x: 202,
+			},
+			want: "202nd",
+		},
+		{
+			name: "303rd",
+			args: args{
+				x: 303,
+			},
+			want: "303rd",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ToOrdinal(tt.args.x); got != tt.want {
+				t.Errorf("ToOrdinal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
