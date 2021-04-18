ac3b4a97e41aac4ed3432b0993f86e6169621c94
Rizal Widyarta Gowandy <rizal.gowandy@tokopedia.com>
2021-04-18 23:03:51 +0700

    Add Benchmark Package

---
+++ pkg/benchmark/uniform.go
@@ -0,0 +1,30 @@
+package generator
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/peractio/gdk/pkg/converter"
+	"github.com/pingcap/go-ycsb/pkg/generator"
+)
+
+type Uniform struct {
+	r *rand.Rand
+	h *generator.Uniform
+}
+
+// nolint:gosec
+func NewUniform(max int) Generator {
+	return &Uniform{
+		r: rand.New(rand.NewSource(time.Now().UnixNano())),
+		h: generator.NewUniform(0, int64(max)),
+	}
+}
+
+func (g *Uniform) Name() string {
+	return "uniform"
+}
+
+func (g *Uniform) Next() string {
+	return converter.ToStr(g.h.Next(g.r))
+}
