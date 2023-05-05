c5153a1f1e41490c9ce6f7255cbcd8c27f22cdad
Rizal Widyarta Gowandy <rizal.gow@gmail.com>
2023-05-05 09:18:28 +0700

    Update NATS Controller

--- pkg/queue/natsx/subscriber_controller.go
+++ pkg/queue/natsx/subscriber_controller.go
@@ -50,6 +50,26 @@ func (c *SubscriberController) AddQueueSubscriber(params []SubscriberParam) erro
 	return nil
 }
 
+func (c *SubscriberController) AddQueue(params []SubscriberParam) error {
+	for _, param := range params {
+		if param.Subscriber == nil {
+			return ErrInvalidSubscriber
+		}
+
+		subscriber, err := c.conn.Subscribe(
+			param.Subject,
+			NewSubscriber(c, param.Subject, param.Queue, param.Subscriber).HandleMessage,
+		)
+		if err != nil {
+			return errors.Wrap(err, "conn.Subscribe")
+		}
+
+		c.subscribers = append(c.subscribers, subscriber)
+	}
+
+	return nil
+}
+
 func (c *SubscriberController) Serve() {
 	finish := make(chan struct{})
 	go func() {
@@ -71,4 +91,18 @@ func (c *SubscriberController) Serve() {
 		close(finish)
 	}()
 	<-finish
+}
+
+func (c *SubscriberController) Close() {
+	var wg sync.WaitGroup
+	for _, v := range c.subscribers {
+		wg.Add(1)
+		con := v
+
+		go func() {
+			defer wg.Done()
+			_ = con.Unsubscribe()
+		}()
+	}
+	wg.Wait()
 }
